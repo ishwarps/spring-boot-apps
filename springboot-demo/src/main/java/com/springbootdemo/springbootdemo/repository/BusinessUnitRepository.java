891cc1b631cf137bb7ad09cbc85f5b0972b1d6eb
panjari <ishwar-shamji.panjari@atos.net>
2019-06-12 16:18:40 +0530

    adding locking changes

--- springboot-demo/src/main/java/com/springbootdemo/springbootdemo/repository/BusinessUnitRepository.java
+++ springboot-demo/src/main/java/com/springbootdemo/springbootdemo/repository/BusinessUnitRepository.java
@@ -3,20 +3,28 @@ package com.springbootdemo.springbootdemo.repository;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.LockModeType;
+
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.springbootdemo.springbootdemo.model.BusinessUnit;
 
 @Repository
-public interface BusinessUnitRepository extends CrudRepository<BusinessUnit, Long>{
-	
-	List<BusinessUnit> findAll();	
+public interface BusinessUnitRepository extends CrudRepository<BusinessUnit, Long> {
+
+	List<BusinessUnit> findAll();
+
 	Optional<BusinessUnit> findById(Long id);
+
 	@SuppressWarnings("unchecked")
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	/*@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Lock(LockModeType.PESSIMISTIC_FORCE_INCREMENT)*/
+	
 	BusinessUnit save(BusinessUnit businessUnit);
+
 	void deleteById(Long id);
-	
-	
-	
+
 }
